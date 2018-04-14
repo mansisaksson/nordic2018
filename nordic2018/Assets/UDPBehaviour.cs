317aedd71c0be8ffdf202f1b374cd5d09d4888cc
Twilice <molkiar@gmail.com>
2018-04-14 23:52:30 +0200

    this is the highest point of gamejam programming

--- nordic2018/Assets/UDPBehaviour.cs
+++ nordic2018/Assets/UDPBehaviour.cs
@@ -75,7 +75,7 @@ public class UDPBehaviour : MonoBehaviour {
             scaleX = transform.localScale.x,
             scaleY = transform.localScale.y,
             scaleZ = transform.localScale.z,
-            angle = transform.localRotation.eulerAngles.x
+            angle = typeName == "Spike" ? transform.localRotation.eulerAngles.x : -90
         };
     }
 }
